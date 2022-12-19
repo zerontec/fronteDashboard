8ee328a323a1d2d92bb35731c1c2c308ff42ed35
zerontec <zerontecnologia1@gmail.com>
2022-12-18 21:53:16 -0400

    modified isRoleProtetected

--- src/untils/isRoleProtect.jsx
+++ src/untils/isRoleProtect.jsx
@@ -1,45 +1,45 @@
-import React, {useEffect} from 'react'
-import { useNavigate} from "react-router-dom";
+import React from 'react'
+import { useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import Swal from 'sweetalert2'
 
 
 
-export default function IsRoleProtect({children}) {
-  
+export default function IsRoleProtect({ children }) {
 
-const navigate = useNavigate()
 
-const { user: currentUser } = useSelector((state) => state);
-const  isRoles = currentUser.roles[0] === 'ROLE_ADMIN'
+  const navigate = useNavigate()
+
+  const { user: currentUser } = useSelector((state) => state);
+  const isRoles = currentUser.roles[0] === 'ROLE_ADMIN'
 
   const message = () => {
     Swal.fire(
       'no esta autorizado !'
 
     )
-   
+
   }
 
 
 
-if(!isRoles){ 
-    
-    
-    
+  if (!isRoles) {
+
+
+
     message()
     navigate('/activos')
-    
-}
+
+  }
 
 
-return(
-<>
-{children}
+  return (
+    <>
+      {children}
 
-</>
+    </>
 
-)
+  )
 
 
 }
